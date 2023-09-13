297a56b36aac32f6240994ec8f8dae9001680dbc
mamun060 <almamun.raju520@gmail.com>
2023-09-13 17:59:32 +0600

    fixed bug

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -8,14 +8,14 @@ function LandingPage() {
         <p className=' text-center text-2xl py-5 font-semibold '>The landing page is unprotected </p>
         <div className=' flex justify-center gap-4'>
           <div>
-            <Link href="/sing-in">
+            <Link href="/sign-in">
               <Button variant="default">
                   Login
               </Button>
             </Link>
           </div>
           <div>
-            <Link href="/sing-up">
+            <Link href="/sign-up">
               <Button variant="destructive">
                   Register
               </Button>
