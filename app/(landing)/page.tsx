4c1defb1b9dfed8ece819ed206eff630c474b609
mamun060 <almamun.raju520@gmail.com>
2023-09-13 17:31:47 +0600

    add clerk authentication for login and register feature

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 function LandingPage() {
   return (
-    <div>
+    <div className=' text-center'>
         <p className=' text-center text-2xl py-5 font-semibold '>The landing page is unprotected </p>
+        <div className=' flex justify-center gap-4'>
+          <div>
+            <Button variant="default">
+              <Link href='/sing-in'>Login</Link>
+            </Button>
+          </div>
+          <div>
+            <Button variant="destructive">
+              <Link href='/sing-up'>Register</Link>
+            </Button>
+          </div>
+        </div>
     </div>
   )
 }
